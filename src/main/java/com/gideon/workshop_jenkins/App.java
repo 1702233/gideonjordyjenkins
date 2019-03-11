6afcd4b08196ad96f1798074763b359082f28b94
1702233 <1702233@student.hu.nl>
2019-03-11 17:12:30 +0100

    2 testen

--- src/main/java/com/gideon/workshop_jenkins/App.java
+++ src/main/java/com/gideon/workshop_jenkins/App.java
@@ -1,5 +1,9 @@
 package com.gideon.workshop_jenkins;
 
+import static org.junit.Assert.assertTrue;
+import java.util.*;
+import org.junit.Test;
+
 /**
  * Hello world!
  *
@@ -11,5 +15,18 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("peins");
 /// asdasdasdh
+    }
+    
+    @Test
+    public void dikkeLul() {
+    	double result = 1 + 1;
+    	assertTrue("berekening hoort 2 te zijn", result  > 2);
+    }
+    	
+    @Test
+    public void kleineLul() {
+    	double result2 = 2 +2;
+    	assertTrue("berekening hoort 4 te zijn", result2  > 4);
+    
     }
 }

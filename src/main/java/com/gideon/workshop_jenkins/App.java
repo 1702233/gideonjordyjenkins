8d9609c5e71ea51bedaeb0f18109e8c1d0aef06f
1702233 <1702233@student.hu.nl>
2019-03-11 17:32:37 +0100

    test try 5

--- src/main/java/com/gideon/workshop_jenkins/App.java
+++ src/main/java/com/gideon/workshop_jenkins/App.java
@@ -14,7 +14,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("peins");
-/// asdasdasdhasdasdas
+/// asdasdasdhasdasdasasdasd
     }
     
     @Test

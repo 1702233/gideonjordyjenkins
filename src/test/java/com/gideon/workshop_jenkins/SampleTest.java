868abd8d68e5c3386bb0b23d2ca1967272f25f07
1702233 <1702233@student.hu.nl>
2019-03-11 17:30:57 +0100

    testen

---
+++ src/test/java/com/gideon/workshop_jenkins/SampleTest.java
@@ -0,0 +1,22 @@
+package com.gideon.workshop_jenkins;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void simpleTest() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+	
+	@Test
+	public void simpleFailedTest() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 1);
+	}
+	
+}
